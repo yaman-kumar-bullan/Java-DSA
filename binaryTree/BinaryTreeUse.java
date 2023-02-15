998b8a470c9d2bfa0d13cacdb6421db4d0ba80c0
Yaman Kumar <yamanbullan952@gmail.com>
2023-02-15 07:46:50 +0000

    Added implementation of takeInput & print, iterative & recursive

--- binaryTree/BinaryTreeUse.java
+++ binaryTree/BinaryTreeUse.java
@@ -1,6 +1,8 @@
 package binaryTree;
-
 import java.util.Scanner;
+
+import dataStructures.EmptyQueueException;
+import dataStructures.QueueUsingLLGenericImplementation;
 
 public class BinaryTreeUse {
 	
@@ -34,11 +36,101 @@ public class BinaryTreeUse {
 		if(root.right != null) printBTAlternate(root.right);
 	}
 	
+	public static BinaryTreeNode<Integer> takeInputBT(Scanner sc ) {
+		System.out.println("Enter root data");
+		int rootData = sc.nextInt();
+		if(rootData == -1) return null;
+
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<>(rootData);
+		root.left = takeInputBT(sc);
+		root.right = takeInputBT(sc);
+
+		return root;
+	}
+
+	public static void printBT(BinaryTreeNode<Integer> root) {
+		if(root == null) return;
+
+		String str = root.data + ":";
+		if(root.left != null) str = str + "L" + root.left.data + ",";
+		if(root.right != null) str = str + "R" + root.right.data;
+		System.out.println(str);
+
+		printBT(root.left);  //Here i can also write if(root.left != null) printBT(root.left);
+		printBT(root.right);
+	}
+
+	public static BinaryTreeNode<Integer> takeInputBTLevelWise() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter root data");
+		int rootData = sc.nextInt();
+		if(rootData == -1) return null;
+
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<>(rootData);
+		QueueUsingLLGenericImplementation<BinaryTreeNode<Integer>> pendingNodes = new QueueUsingLLGenericImplementation<>();
+		pendingNodes.enqueue(root);
+
+		while(!pendingNodes.isEmpty()) {
+			BinaryTreeNode<Integer> front;
+			try {
+				front = pendingNodes.dequeue();
+			} catch (EmptyQueueException e) {
+				return null;
+			}
+
+			System.out.println("Enter left child of " + front.data + ":");
+			int leftChildData = sc.nextInt();
+			if(leftChildData != -1) {
+				BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<>(leftChildData);
+				pendingNodes.enqueue(leftChild);
+				front.left = leftChild;
+			}
+
+			System.out.println("Enter right child of " + front.data + ":");
+			int rightChildData = sc.nextInt();
+			if(rightChildData != -1) {
+				BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<>(rightChildData);
+				pendingNodes.enqueue(rightChild);
+				front.right = rightChild;
+			}
+		}
+
+		return root;
+	}
+
+	public static void printBTLevelWise(BinaryTreeNode<Integer> root) {
+		QueueUsingLLGenericImplementation<BinaryTreeNode<Integer>> pendingNodes = new QueueUsingLLGenericImplementation<>();
+		pendingNodes.enqueue(root);
+
+		while(!pendingNodes.isEmpty()) {
+			BinaryTreeNode<Integer> front;
+			try {
+				front = pendingNodes.dequeue();
+			} catch (EmptyQueueException e) {
+				return;
+			}
+
+			if(front == null) return;
+
+			String str = front.data + ":";
+			if(front.left != null) {
+				str = str + "L" + front.left.data + ",";
+				pendingNodes.enqueue(front.left); 
+			}
+
+			if(front.right != null) {
+				str = str + "R" + front.right.data;
+				pendingNodes.enqueue(front.right); 
+			}
+
+			System.out.println(str);
+		}
+	}
+
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		BinaryTreeNode<Integer> root = takeBTInputAlternate(sc);
-		
-		
+		BinaryTreeNode<Integer> root = takeInputBTLevelWise();
+		printBTLevelWise(root);
 	}
 }
