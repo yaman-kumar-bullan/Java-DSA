81e6b628ea064c425361a30923ff307875ef8d62
Yaman Kumar <yamanbullan952@gmail.com>
2023-02-26 07:09:24 +0000

    Dummy commit

---
+++ binaryTree/TriplePair.java
@@ -0,0 +1,13 @@
+package binaryTree;
+
+public class TriplePair<X,Y,Z> {
+    X first;
+    Y second;
+    Z third;
+
+    public TriplePair(X first, Y second, Z third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+}
